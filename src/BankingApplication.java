2642dfb9c7c944ede52570be132f26375b043dde
nawaz quazi <nawazquazi356@gmail,com>
2023-01-10 14:12:59 +0530

    Java Banking Application

---
+++ src/BankingApplication.java
@@ -0,0 +1,6 @@
+public class BankingApplication {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("BA0001", "XYZ");
+        account.showMenu();
+    }
+}
